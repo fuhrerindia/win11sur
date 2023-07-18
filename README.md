fc0ff40359cf29590e956ea74adca9f6afcd2fa8
Paurush Sinha <paurush.sinha.d@gmail.com>
2023-07-18 17:33:40 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Win11Sur
+Themes and Wallpaper installer for my personal use, this will make Ubuntu look like MacOS with Windows 11+MacOS Icons.
+
+## Themes Being Installed
+- [WhiteSur Gtk Theme](https://www.gnome-look.org/p/1403328) by [Vince](https://github.com/vinceliuice)
+- [Win10Sur icon theme](https://www.gnome-look.org/p/1440037) by [yeyushengfan258](https://github.com/yeyushengfan258/Win10Sur-icon-theme)
+- [McMojave cursors](https://www.gnome-look.org/p/1355701) by [Vince](https://github.com/vinceliuice/McMojave-cursors)
+
+## Extension Recommended for Better Experience
+- [Emoji Selector](https://extensions.gnome.org/extension/1162/emoji-selector/)
+- [Compiz Window Effect](https://extensions.gnome.org/extension/3210/compiz-windows-effect/)
+- [Move Clock](https://extensions.gnome.org/extension/2/move-clock/)
+- [Lamp Effect](https://extensions.gnome.org/extension/3740/compiz-alike-magic-lamp-effect/)
+- [Blur My Shell](https://extensions.gnome.org/extension/3193/blur-my-shell/)
---
+++ install.sh
@@ -0,0 +1,31 @@
+sudo apt update
+sudo apt install gnome-tweaks
+if [ -d "~/.themes" ]; then
+    echo "DIRECTORY ALREADY FOUND, HENCE MOVING FILES INTO IT"
+else
+    mkdir ~/.themes
+fi
+if [ -d "~/.icons" ]; then
+    echo "DIRECTORY ALREADY FOUND, HENCE MOVING FILES INTO IT"
+else
+    mkdir ~/.icons
+fi
+cp -r ./assets/themes/* ~/.themes/
+cp -r ./assets/icons/* ~/.icons/
+
+# ENABLING SHELL THEMES
+sudo apt-get install gnome-shell-extensions
+gnome-extensions enable user-themes@gnome-shell-extensions.gcampax.github.com
+gsettings set org.gnome.shell.extensions.user-themes enabled true
+gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-Dark"
+gsettings set org.gnome.desktop.interface icon-theme "Win10Sur"
+gsettings set org.gnome.desktop.interface cursor-theme "McMojave Cursors"
+cp ./assets/wallpaper/* ~/.local/share/backgrounds/
+gsettings set org.gnome.desktop.background picture-uri ~/.local/share/backgrounds/1.png
+
+echo -e "\n\nSome additional extensions are recommended, links to them are below."
+echo -e "\e]8;;https://extensions.gnome.org/extension/1162/emoji-selector/\aEmoji Selector\e]8;;\a"
+echo -e "\e]8;;https://extensions.gnome.org/extension/3210/compiz-windows-effect/\aCompiz Window Effect\e]8;;\a"
+echo -e "\e]8;;https://extensions.gnome.org/extension/2/move-clock/\aMove Clock\e]8;;\a"
+echo -e "\e]8;;https://extensions.gnome.org/extension/3740/compiz-alike-magic-lamp-effect/\aLamp Effect\e]8;;\a"
+echo -e "\e]8;;https://extensions.gnome.org/extension/3193/blur-my-shell/\aBlur My Shell\e]8;;\a"
